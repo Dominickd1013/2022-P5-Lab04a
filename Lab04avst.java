09c7f0d790b08dec1d6a02e322a378023576880d
Dominick <domenico.damato@vtsd.com>
2022-10-03 12:19:37 -0400

    This is lab 04 completed.

--- Lab04avst.java
+++ Lab04avst.java
@@ -1,43 +1,52 @@
 // Lab04avst.java
 // The Mortgage Payment Program
 // This the student, starting version of the Lab04a assignment.
-
-
 public class Lab04avst
 {
     public static void main(String[] args)
     {
         System.out.println("Lab04a, Student Version\n");
 
+        // Variables
         double principal   = 250000; // P
         double annualRate  = 4.85;
         double monthlyRate = (annualRate / 100) / 12; // R
         double numYears    = 30;
         double numMonths   = 360; // N
 
-        // Monthly payment output
-
-        double numerator      = (monthlyRate * Math.pow(1 + monthlyRate, numMonths)); // gives me the numerator
-        double denominator    = (Math.pow(1 + monthlyRate, numMonths) - 1) ; // gives me the denominator
-        double monthlyPayment = (((numerator/denominator) * principal)); // The monthly payment
+        // Monthly payment
+        double numerator   = (monthlyRate * Math.pow(1 + monthlyRate, numMonths)); // Gives me the numerator
+        double denominator = (Math.pow(1 + monthlyRate, numMonths) - 1) ; // Gives me the denominator
+        double monthlyPaymentUnRounded = (((numerator/denominator) * principal)); // The monthly payment
+        double monthlyPaymentRounded   = (Math.ceil(monthlyPaymentUnRounded*100)/100);
 
         // Total payments
-
-        double totalPayment = (monthlyPayment  * numMonths);
+        double totalPaymentUnRounded = (monthlyPaymentUnRounded  * numMonths);
+        double totalPaymentRounded   = (Math.ceil(totalPaymentUnRounded * 100)/100);
 
         // Total interest
-
-        double totalInterest = (totalPayment - 360 * (monthlyPayment * monthlyRate));
-
-
+        double totalInterestUnRounded = (totalPaymentUnRounded - principal);
+        double totalInterestRounded   = (Math.ceil(totalInterestUnRounded * 100)/100);
 
 
-        System.out.println(principal);
-        System.out.println(annualRate);
+        System.out.print("Principal:        $");
+        System.out.print(principal);
         System.out.println();
-        System.out.println(monthlyPayment);
-        System.out.println(totalPayment);
-        System.out.println(totalInterest);
+        System.out.print("Annual Rate:      ");
+        System.out.print(annualRate);
+        System.out.println("%");
+        System.out.print("Number of Years:  ");
+        System.out.print(numYears);
+        System.out.println();
+        System.out.print("Monthly Payment:  $");
+        System.out.print(monthlyPaymentRounded);
+        System.out.println();
+        System.out.print("Total Payments:   $");
+        System.out.print(totalPaymentRounded);
+        System.out.println();
+        System.out.print("Total Interest:   $");
+        System.out.print(totalInterestRounded);
+
 
     }
 }
